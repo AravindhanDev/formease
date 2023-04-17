5a47a416496421dfa97824c9eee5c0242d773ed9
NoobieUs37 <NoobieUs@protonmail.com>
2023-04-17 18:46:17 +0300

    some changes

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -3,7 +3,7 @@ import FormBox from "@/components/Home/FormBox"
 import FormNavbar from "@/components/Home/FormNavbar"
 import { useEffect } from "react"
 
-function forms() {
+function Forms() {
     useEffect(() => {
         document.body.style.display = "block"
         document.documentElement.style.visibility = "visible"
@@ -12,7 +12,7 @@ function forms() {
     return (
         <>
             <FormNavbar />
-            <section className="my-7 flex justify-center">
+            <section className="my-7 flex justify-center bg-gray-100">
                 <div className="w-3/5 md:w-4/5 sm:p-5 xs:p-5 sm:w-full xs:w-full flex flex-col">
                     <h3 className="font-medium">Start a new form</h3>
                     <div className="flex flex-wrap">
@@ -41,4 +41,4 @@ function forms() {
     )
 }
 
-export default forms
+export default Forms
